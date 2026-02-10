d50e84ded7bbf712995428513d79488ad82c2c00
Kaucher Islam Forhad <mdkaucher425@gmail.com>
2026-02-10 13:00:55 -0500

    The innitial setup file.

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 sudo apt install macchanger -y
 sudo apt install python3 -y
 
-python3 -m venv ShadowSwitch
-source ShadowSwitch/bin/activate
+python3 -m venv ss_venv
+source ss_venv/bin/activate
 pip3 install -r requirements.txt
