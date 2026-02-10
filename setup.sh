731ef48925ee05dcdd9fb6c2ea5a89e9bac9a58b
Kaucher Islam Forhad <mdkaucher425@gmail.com>
2026-02-10 13:28:43 -0500

    The innitial setup file.
    Added comments for better understanding.

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-sudo apt install macchanger -y
-sudo apt install python3 -y
+# For installing macchanger & python if incase the user dosen't have it.
+sudo apt install macchanger python3 -y
 
+# Adding a virtul environment so that it don't have to use "--break-system-packages".
 python3 -m venv ss_venv
+
+# Activating the virtual environment.
 source ss_venv/bin/activate
+
+# Installing the requirements.
 pip3 install -r requirements.txt
+
+# Giving ShadowSwitch exicutable permissions, just in case.
+chmod +x ShadowSwitch.py
